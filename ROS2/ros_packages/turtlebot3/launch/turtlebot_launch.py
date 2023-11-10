8e440fa69211af30468a05089819c63b3d9b1c5f
SimonWilmotsIIW <simon.wilmots@student.uhasselt.be>
2023-11-10 21:02:34 +0100

    Implemented tf2_ros to fix RVIZ2 issue

--- ROS2/ros_packages/turtlebot3/launch/turtlebot_launch.py
+++ ROS2/ros_packages/turtlebot3/launch/turtlebot_launch.py
@@ -2,6 +2,7 @@ import os
 import launch
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from ament_index_python.packages import get_package_share_directory
 from webots_ros2_driver.webots_controller import WebotsController
 
@@ -9,11 +10,15 @@ from webots_ros2_driver.webots_controller import WebotsController
 def generate_launch_description():
     package_dir = get_package_share_directory('turtlebot3')
     robot_description_path = os.path.join(package_dir, 'resource', 'simonwilmots_turtlebot3.urdf')
+    ros2_control_params = os.path.join(package_dir, 'resource', 'ros2control.yaml')
+    rviz2_config = os.path.join(package_dir, 'rviz2', 'rviz2.rviz')
 
     turtlebot_driver = WebotsController(
         robot_name='SimonWilmots_TurtleBot3',
         parameters=[
-            {'robot_description': robot_description_path},
+            {'robot_description': robot_description_path,
+             'set_robot_state_publisher': True},
+            ros2_control_params,
         ]
     )
 
@@ -25,11 +30,35 @@ def generate_launch_description():
     #         ('params_file', nav2_params),
     #     ],
     # )
-
+    # todo https://github.com/turtlebot/turtlebot4_desktop/tree/humble
+        
+    #description_launch = PathJoinSubstitution(
+    #    [package_dir, 'launch', 'robot_description.launch.py']
+    #)
+    
     rviz2 = Node(
         package='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz2_config],
         executable='rviz2',
-        output='screen'
+        #remappings=[('/tf', 'tf'), ('tf_static', 'tf_static'),],
+        output='screen',
+    )
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{
+            'robot_description': robot_description_path
+        }],
+    )
+
+    footprint_publisher = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        output='screen',
+        arguments=['0', '0', '0', '0', '0', '0', 'base_link', 'LDS_01'],
     )
 
     shutdown_handler = launch.actions.RegisterEventHandler(
@@ -43,5 +72,7 @@ def generate_launch_description():
         turtlebot_driver,
         # nav2_node
         rviz2,
+        footprint_publisher,
+        robot_state_publisher,
         shutdown_handler
     ])
